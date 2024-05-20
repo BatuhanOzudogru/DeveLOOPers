764c3177b2ac3742c9aabe68f5c9fb1a7dc25171
BatuhanOzudogru <batuhan.ozudogru@hotmail.com>
2024-05-20 17:21:27 +0300

    Add properties to article model & migrate

--- deveLOOPers/app/models/article.rb
+++ deveLOOPers/app/models/article.rb
@@ -1,2 +1,12 @@
 class Article < ApplicationRecord
+    enum status: {unpublished:0,published:1}
+
+    after_initialize :set_defaiult_status, if: :new_record?
+
+    private
+
+    def set_defaiult_status
+        selt.status ||= :unpublished
+    end
+    
 end
--- deveLOOPers/db/migrate/20240520141524_create_articles.rb
+++ deveLOOPers/db/migrate/20240520141524_create_articles.rb
@@ -1,9 +1,9 @@
 class CreateArticles < ActiveRecord::Migration[7.1]
   def change
     create_table :articles do |t|
-      t.string :title
-      t.text :content
-      t.integer :status
+      t.string :title, limit:140
+      t.text :content, default:"content will be here soon"
+      t.integer :status, default:0
 
       t.timestamps
     end
