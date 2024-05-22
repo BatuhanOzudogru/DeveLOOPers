d684df013f85a5542089216c279a4aa97331da57
BatuhanOzudogru <batuhan.ozudogru@hotmail.com>
2024-05-22 16:38:33 +0300

    Add rich text editot to content

--- deveLOOPers/app/models/article.rb
+++ deveLOOPers/app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ApplicationRecord
     enum status: {unpublished:0,published:1}
     validates :title, presence:true, uniqueness: true
     after_initialize :set_default_status, if: :new_record?
-
+    has_rich_text :content
     private
 
     def set_default_status
