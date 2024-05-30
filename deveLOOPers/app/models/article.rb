242f0420e6be6431511a2edb7de31c3258c0e82e
BatuhanOzudogru <batuhan.ozudogru@hotmail.com>
2024-05-30 22:52:50 +0300

    Add RemoveStatusFromArticles migrate

--- deveLOOPers/app/models/article.rb
+++ deveLOOPers/app/models/article.rb
@@ -1,5 +1,4 @@
 class Article < ApplicationRecord
-    enum status: {unpublished:0,published:1}
     validates :title, presence:true, uniqueness: true
     after_initialize :set_default_status, if: :new_record?
     has_rich_text :content
--- deveLOOPers/db/schema.rb
+++ deveLOOPers/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_22_125140) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_30_194907) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,7 +55,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_22_125140) do
   create_table "articles", force: :cascade do |t|
     t.string "title", limit: 140
     t.text "content"
-    t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
