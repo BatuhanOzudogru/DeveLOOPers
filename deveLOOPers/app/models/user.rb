481a43a82475189dc20837761618753fe053ee86
BatuhanOzudogru <batuhan.ozudogru@hotmail.com>
2024-06-12 15:41:05 +0300

    Add omniauth gem fot github sign in

--- deveLOOPers/app/models/user.rb
+++ deveLOOPers/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[github]
 
   before_validation :set_nickname, on: :create
 
@@ -8,6 +8,19 @@ class User < ApplicationRecord
 
   has_many :articles, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+
+      unless user
+       user = User.create(
+           email: data['email'],
+           password: Devise.friendly_token[0,20]
+        )
+      end
+    user
+end
 
   private
 
--- deveLOOPers/config/initializers/devise.rb
+++ deveLOOPers/config/initializers/devise.rb
@@ -271,7 +271,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :github, 'Ov23liximGbm0dmvE42B', '90f6421ce359422320b3d08b87f2e2ac2de72f44'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
